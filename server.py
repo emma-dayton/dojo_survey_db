03c466eb8377f5bc001fa37df3a3889c1a1a8610
Emma Dayton <emma.dayton85@gmail.com>
2019-03-13 08:47:58 -0700

    create survey.html, copy base layout file, add debug and home app.route to server.py

--- server.py
+++ server.py
@@ -2,3 +2,13 @@ from flask import Flask, render_template, request, redirect, session
 from mysqlconnection import connectToMySQL
 app = Flask(__name__)
 app.secret_key = '4a136bc896d8e3657d1799320bb2aa37'
+
+
+@app.route('/')
+def survey():
+    return render_template('survey.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
