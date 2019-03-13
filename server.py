3d2d41350aeccd2014225d5fa98e2a7df46802ec
Emma Dayton <emma.dayton85@gmail.com>
2019-03-13 09:35:53 -0700

    copy bootstrap directory to project, update layout and survey html to display basic survey, update server.py to get database info

--- server.py
+++ server.py
@@ -6,7 +6,16 @@ app.secret_key = '4a136bc896d8e3657d1799320bb2aa37'
 
 @app.route('/')
 def survey():
-    return render_template('survey.html')
+    session['title'] = 'Dojo Survey'
+    db = connectToMySQL('dojo_survey')
+    query1 = 'SELECT * FROM location'
+    query2 = 'SELECT * FROM languages'
+    locations = db.query_db(query1)
+    db = connectToMySQL('dojo_survey')
+    languages = db.query_db(query2)
+    print(locations)
+    print(languages)
+    return render_template('survey.html', locations=locations, languages=languages)
 
 
 
