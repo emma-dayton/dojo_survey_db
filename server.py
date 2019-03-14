5494bc82048faf4d951607a934e2d8f17077b20f
Emma Dayton <emma.dayton85@gmail.com>
2019-03-13 19:54:12 -0700

    make the submit route do 3 queries to get foreign keys for users db, move button on results template

--- server.py
+++ server.py
@@ -6,6 +6,8 @@ app.secret_key = '4a136bc896d8e3657d1799320bb2aa37'
 
 @app.route('/')
 def survey():
+    if 'data' in session:
+        session.pop('data')
     session['title'] = 'Dojo Survey'
     db = connectToMySQL('dojo_survey')
     query1 = 'SELECT * FROM location'
@@ -25,17 +27,35 @@ def survey_says():
     lang = request.form['language']
     comment = request.form['comment']
     data = {'name':name, 'dojo':dojo, 'lang':lang, 'comment':comment}
+    session['data'] = data
     return render_template('results.html', data=data)
 
 @app.route('/submit', methods=['POST'])
 def submit():
     db = connectToMySQL('dojo_survey')
-    data = request.form['data']
-    if 'lang' in data:
-        query = "INSERT INTO users (name) VALUES(%(name)s)"
-    else:
-        query = "INSERT INTO users (name) VALUES(%(name)s)"
-    print(data)
+    query = "SELECT id FROM location WHERE city = %(city)s;"
+    city = session['data']['dojo'].split(',')
+    city = {'city':city[0]}
+    loc_id = db.query_db(query, city)
+    loc_id = loc_id[0]['id']
+    db = connectToMySQL('dojo_survey')
+    language = session['data']['lang']
+    lang = {'lang':language}
+    query = "SELECT id FROM languages WHERE lang_name = %(lang)s;"
+    lang_id = db.query_db(query, lang)
+    print(lang_id, '++++++++++++++++')
+    lang_id = lang_id[0]['id']
+    db = connectToMySQL('dojo_survey')
+    data = {
+    'comment':session['data']['comment'],
+    'name':session['data']['name'],
+    'loc_id':loc_id,
+    'lang_id':lang_id
+    }
+    query = """INSERT INTO users (name, comment, location_id, languages_id)
+    VALUES (%(name)s, %(comment)s, %(loc_id)s, %(lang_id)s)"""
+    huh = db.query_db(query, data)
+    print(huh, '+++++++++++++++++++++++++++++++++')
     return redirect('/')
 
 
